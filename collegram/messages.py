af49d4cc74006d464a85933c5e8f176f215996cd
TLouf <tlouf+pro@pm.me>
2023-11-15 12:32:24 +0100

    Fix `get_channel_messages` at first channel's message

--- collegram/messages.py
+++ collegram/messages.py
@@ -17,11 +17,13 @@ def get_channel_messages(client: TelegramClient, channel: str | Channel, dt_from
     date_to exclusive
     '''
     offset_id = 0
+    limit = 10000
     all_messages = []
     total_messages = 0
     keep_going = True
 
     while keep_going:
+        chunk_messages = []
         logger.info(f"Current Offset ID is: {offset_id}; Total Messages: {total_messages}")
         # Splitting by 10k chunks in case of disconnection, to at least save something (TODO: save)
         # Telethon docs are misleading, `offset_date` is in fact a datetime.
@@ -29,23 +31,25 @@ def get_channel_messages(client: TelegramClient, channel: str | Channel, dt_from
             entity=channel,
             offset_date=dt_to,
             offset_id=offset_id,
-            limit=10000,
+            limit=limit,
         )
-
-        if not messages:
-            break
 
         for message in messages:
             # Take messages in until we've gone further than `date_until` in the past
             # (works because HistoryRequest gets messages in reverse chronological order
             # by default)
             if message.date >= dt_from:
-                all_messages.append(message)
+                chunk_messages.append(message)
             else:
                 keep_going = False
                 break
 
+        if len(chunk_messages) < limit:
+            # Needed when reaching first message ever posted
+            keep_going = False
+
         offset_id = message.id
+        all_messages.extend(chunk_messages)
         total_messages = len(all_messages)
 
     return all_messages
