4153103292c5f87494db58316b9fb426f16c9f06
TLouf <tlouf+pro@pm.me>
2024-01-09 16:26:28 +0100

    Add user data flatenning functions

--- collegram/users.py
+++ collegram/users.py
@@ -1,16 +1,19 @@
 from __future__ import annotations
 
 import logging
-from typing import TYPE_CHECKING
+import typing
 
+import polars as pl
 import telethon.sync
 from telethon.errors import ChatAdminRequiredError
 from telethon.tl.functions.channels import GetParticipantsRequest
-from telethon.tl.types import ChannelParticipantsSearch
+from telethon.tl.types import ChannelParticipantsSearch, User
 
-if TYPE_CHECKING:
+from collegram.utils import PY_PL_DTYPES_MAP
+
+if typing.TYPE_CHECKING:
     from telethon import TelegramClient
-    from telethon.tl.types import Channel, User
+    from telethon.tl.types import Channel
 
 logger = logging.getLogger(__name__)
 
@@ -58,3 +61,28 @@ def get_channel_users(client: TelegramClient, channel: str | Channel, anon_func)
         p.id = anon_func(p.id)
         users.append(p)
     return users
+
+CHANGED_USER_FIELDS = {'id': pl.Utf8}
+DISCARDED_USER_FIELDS = (
+    '_', 'contact', 'mutual_contact', 'close_friend', 'first_name', 'last_name',
+    'username', 'usernames', 'phone', 'restriction_reason', 'photo', 'emoji_status',
+    'color', 'status',
+)
+
+def flatten_dict(p: dict):
+    flat_p = p.copy()
+    for f in DISCARDED_USER_FIELDS:
+        flat_p.pop(f)
+    # TODO status?
+    return flat_p
+
+def get_pl_schema():
+    user_schema = {}
+    annots = User.__init__.__annotations__
+    for arg in set(annots.keys()).difference(DISCARDED_USER_FIELDS):
+        dtype = annots[arg]
+        inner_dtype = typing.get_args(dtype)
+        inner_dtype = inner_dtype[0] if len(inner_dtype) > 0 else dtype
+        user_schema[arg] = PY_PL_DTYPES_MAP.get(inner_dtype)
+    user_schema.update(CHANGED_USER_FIELDS)
+    return user_schema
