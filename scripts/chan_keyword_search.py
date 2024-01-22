df128e3b4ee97b5cf1f29dff140cc15a22c17633
TLouf <tlouf+pro@pm.me>
2024-01-22 16:31:37 +0100

    Print out how many channels were found

--- scripts/chan_keyword_search.py
+++ scripts/chan_keyword_search.py
@@ -19,8 +19,10 @@ if __name__ == '__main__':
     searched_keywords = set(searched_kw_path.read_text().strip().split("\n"))
     keywords_to_search = keywords.difference(searched_keywords)
     for i, kw in enumerate(keywords_to_search):
-        channels = channels.union(collegram.channels.search_from_tgdb(client, kw)).union(collegram.channels.search_from_api(client, kw))
+        tgdb_chans = collegram.channels.search_from_tgdb(client, kw)
+        api_chans = collegram.channels.search_from_api(client, kw)
+        channels = channels.union(tgdb_chans).union(api_chans)
         output_file.write_text("\n".join(channels))
         searched_keywords.add(kw)
         searched_kw_path.write_text("\n".join(searched_keywords))
-        print(f"{i} / {len(keywords_to_search)}")
+        print(f"{i} / {len(keywords_to_search)}: {len(tgdb_chans)} from TGDB, {len(api_chans)} from API")
