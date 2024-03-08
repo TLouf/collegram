8fb17f20696bc6b570f7457d2444c384badeb6dc
TLouf <loufthomas@gmail.com>
2024-03-08 11:44:49 +0100

    Expose common classes and functions to top-level

--- collegram/__init__.py
+++ collegram/__init__.py
@@ -1,6 +1,8 @@
 import logging
 
 from . import channels, client, json, media, messages, paths, text, users, utils
+from .paths import ChannelPaths, ProjectPaths
+from .utils import HMAC_anonymiser, UniquePriorityQueue, get_last_modif_time
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
@@ -14,4 +16,9 @@ __all__ = [
     "utils",
     "json",
     "text",
+    "ChannelPaths",
+    "ProjectPaths",
+    "HMAC_anonymiser",
+    "UniquePriorityQueue",
+    "get_last_modif_time",
 ]
