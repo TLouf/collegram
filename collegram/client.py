bead5646cd67f598a935bc6e2392bb7bbfd72b2f
TLouf <loufthomas@gmail.com>
2024-07-11 14:18:10 +0200

    Add util to get StringSession from other kind

--- collegram/client.py
+++ collegram/client.py
@@ -1,8 +1,8 @@
 import logging
 
-import telethon.sync
 from telethon import TelegramClient
 from telethon.errors import SessionPasswordNeededError
+from telethon.sessions import Session, StringSession
 
 logger = logging.getLogger(__name__)
 
@@ -21,3 +21,10 @@ def connect(api_id, api_hash, phone_nr, session="anon", **client_kwargs):
             client.sign_in(password=input("Password: "))
 
     return client
+
+
+def string_session_from(session: Session):
+    s = StringSession()
+    s.set_dc(session.dc_id, session.server_address, session.port)
+    s.auth_key = session.auth_key
+    return s
