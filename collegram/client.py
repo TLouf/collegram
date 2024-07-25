89340cd31061d31c3b24b6526c4a10bfd1178b9f
TLouf <loufthomas@gmail.com>
2024-07-25 18:00:07 +0200

    simplify `connect`

--- collegram/client.py
+++ collegram/client.py
@@ -1,7 +1,6 @@
 import logging
 
 from telethon import TelegramClient
-from telethon.errors import SessionPasswordNeededError
 from telethon.sessions import Session, StringSession
 
 logger = logging.getLogger(__name__)
@@ -9,19 +8,8 @@ logger = logging.getLogger(__name__)
 
 def connect(api_id, api_hash, phone_nr, session="anon", **client_kwargs):
     client = TelegramClient(session, api_id, api_hash, **client_kwargs)
-    client.start()
+    client.start(phone_nr)
     logger.info("Client Created")
-
-    if not client.loop.run_until_complete(client.is_user_authorized()):
-        phone_nr = phone_nr
-        client.loop.run_until_complete(client.send_code_request(phone_nr))
-        try:
-            client.loop.run_until_complete(
-                client.sign_in(phone_nr, input("Enter the code: "))
-            )
-        except SessionPasswordNeededError:
-            client.loop.run_until_complete(client.sign_in(password=input("Password: ")))
-
     return client
 
 
