0862edda3b68399d0cc3f854f70904356698bf12
TLouf <loufthomas@gmail.com>
2024-07-31 16:16:11 +0200

    Work on raw, non-anoned data in lang detect

--- collegram/text.py
+++ collegram/text.py
@@ -7,7 +7,7 @@ if TYPE_CHECKING:
     from lingua import LanguageDetector
 
 
-def get_clean_chan_text(full_channel_d: dict, inverse_anon_map: dict):
+def get_clean_chan_text(full_channel_d: dict):
     # TODO: maybe add a sample of messages?
     channel_d = [
         c
@@ -15,7 +15,6 @@ def get_clean_chan_text(full_channel_d: dict, inverse_anon_map: dict):
         if c["id"] == full_channel_d["full_chat"]["id"]
     ][0]
     title = channel_d.get("title", "")
-    title = inverse_anon_map.get(title, title)
     clean_text = f"{title}. {full_channel_d['full_chat'].get('about', '')}"
     hash_at_pattern = r"(?:^|\B)((@|#)\w+)(?:$|\b)"
     url_pattern = r"(?:^|\s)(\S+\/t.co\/\S+)(?:$|\b)"
@@ -25,10 +24,10 @@ def get_clean_chan_text(full_channel_d: dict, inverse_anon_map: dict):
 
 
 def detect_chan_lang(
-    full_channel_d: dict, inverse_anon_map: dict, lang_detector: LanguageDetector
+    full_channel_d: dict, lang_detector: LanguageDetector
 ) -> str | None:
     # TODO:  handle multilingual?
-    text = get_clean_chan_text(full_channel_d, inverse_anon_map)
+    text = get_clean_chan_text(full_channel_d)
     lang = lang_detector.detect_language_of(text)
     lang_code = None if lang is None else lang.iso_code_639_1.name
     return lang_code
