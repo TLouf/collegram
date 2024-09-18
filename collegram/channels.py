a05e8019e4580a0a6dd457bc9e4f549d138b1575
TLouf <loufthomas@gmail.com>
2024-09-18 11:11:21 +0200

    Reintroduce more complete `get_explo_priority`

--- collegram/channels.py
+++ collegram/channels.py
@@ -258,6 +258,36 @@ def anon_full_dict(full_dict: dict, anonymiser: HMAC_anonymiser, safe=True):
     return full_dict
 
 
+def get_explo_priority(
+    lang_code: str,
+    messages_count: int,
+    participants_count: int,
+    lifespan_seconds: int,
+    distance_from_core: int,
+    nr_forwarding_channels: int,
+    nr_recommending_channels: int,
+    lang_priorities: dict,
+    acty_slope: float = 1,
+    acty_inflexion: float = 0.1,  # 10 messages per day for channel of 100 users
+):
+    # Returns a score between 0 and 1. Here the lowest the returned score, the more
+    # priority the channel is given for message collection.
+    lang_score = lang_priorities.get(lang_code, 1)
+    acty_per_user_day = (
+        messages_count / participants_count / (lifespan_seconds / 3600 / 24)
+    )
+    acty_score = 1 / (1 + (acty_inflexion / acty_per_user_day) ** acty_slope)
+    # With the following, for instance if channel is forwarded from by two core channels
+    # (dist = 0), it will be prioritised over other core channels. And if channel is
+    # recommended in / fowarded from n times more than another at the same distance from
+    # the core, it will have a priority ~n times smaller.
+    central_score = (distance_from_core + 1) / (
+        1 + nr_recommending_channels + nr_forwarding_channels
+    )
+    # We threshold acty_score to 1e-3 so that it doesn't dominate all others.
+    return lang_score * max(acty_score, 1e-3) * min(central_score, 1)
+
+
 def save(
     chan_data: dict,
     project_paths: ProjectPaths,
