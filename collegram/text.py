03de011737a6cefd6afe931742472ac02cd930ef
TLouf <loufthomas@gmail.com>
2024-12-13 11:46:07 +0100

    Add back titlte deanon in language detection

--- collegram/text.py
+++ collegram/text.py
@@ -7,7 +7,7 @@ if TYPE_CHECKING:
     from lingua import LanguageDetector
 
 
-def get_chan_text(full_channel_d: dict):
+def get_chan_text(full_channel_d: dict, inverse_anon_map: dict):
     # TODO: maybe add a sample of messages?
     channel_d = [
         c
@@ -15,6 +15,7 @@ def get_chan_text(full_channel_d: dict):
         if c["id"] == full_channel_d["full_chat"]["id"]
     ][0]
     title = channel_d.get("title", "")
+    title = inverse_anon_map.get(title, title)
     text = f"{title}. {full_channel_d['full_chat'].get('about', '')}"
     return text
 
@@ -28,10 +29,10 @@ def clean_text(s: str):
 
 
 def detect_chan_lang(
-    full_channel_d: dict, lang_detector: LanguageDetector
+    full_channel_d: dict, inverse_anon_map: dict, lang_detector: LanguageDetector
 ) -> str | None:
     # TODO:  handle multilingual?
-    text = get_chan_text(full_channel_d)
+    text = get_chan_text(full_channel_d, inverse_anon_map)
     text = clean_text(text)
     lang = lang_detector.detect_language_of(text)
     lang_code = None if lang is None else lang.iso_code_639_1.name
