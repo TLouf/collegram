cfd5310049771d7b77c917d59dd9a19aac91ccc6
TLouf <loufthomas@gmail.com>
2025-06-30 15:19:45 +0200

    update to base on username

--- scripts/media_dl.py
+++ scripts/media_dl.py
@@ -9,13 +9,9 @@ import collegram
 if __name__ == "__main__":
     load_dotenv()
 
-    chan_id = 123
-    msg_id = 1
+    channel_username = "username"
+    msg_id = 3301
     output_path = Path(".")
-    paths = collegram.paths.ProjectPaths()
-    channel_d = collegram.channels.load(str(chan_id), paths)
-    chat_d = collegram.channels.get_matching_chat_from_full(channel_d, chan_id)
-    access_hash = chat_d["access_hash"]
 
     client = TelegramClient(
         "session_name",
@@ -25,7 +21,8 @@ if __name__ == "__main__":
     client = client.start(os.environ["PHONE_NUMBER"])
 
     input_chan = collegram.channels.get_input_peer(
-        client, channel_id=chan_id, access_hash=access_hash
+        client,
+        channel_username=channel_username,
     )
 
     media = collegram.media.download_from_message_id(
